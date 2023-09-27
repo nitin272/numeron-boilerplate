cd7859b90c73f815cfa60f54369c8c9b3476eac5
nitin272 <142768664+nitin272@users.noreply.github.com>
2023-09-27 09:25:45 +0530

    Update score.js

--- score.js
+++ score.js
@@ -1,1 +1,11 @@
 // Iteration 5: Store the player score and display it on the game over screen
+// Iteration 5: Store the player score and display it on the game over screen
+var playagain = document.getElementById('play-again-button');
+playagain.onclick = () => {
+    location.href = './game.html';
+}
+
+const scoreboard = document.getElementById('score-board');
+let score = localStorage.getItem('score');
+
+scoreboard.innerHTML = score;
